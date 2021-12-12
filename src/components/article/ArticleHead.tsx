da327ad1828ada9878af7216c7e51797d55f4496
Ciffelia <mc.prince.0203@gmail.com>
2021-12-12 16:04:07 +0900

    Add JSON-LD data

--- src/components/article/ArticleHead.tsx
+++ src/components/article/ArticleHead.tsx
@@ -1,3 +1,6 @@
+import Head from 'next/head';
+import { jsonLdScriptProps } from 'react-schemaorg';
+import { BlogPosting, BreadcrumbList } from 'schema-dts';
 import { NextSeo } from 'next-seo';
 import { ArticleMetadata } from '@/types/ArticleMetadata';
 
@@ -15,28 +18,59 @@ const ArticleHead: React.VFC<Props> = ({
     modifiedAt,
     ogpImageUrl,
   },
-}) => (
-  <>
-    <NextSeo
-      title={title}
-      description={description}
-      canonical={`https://blog.ciffelia.com/article/${slug}`}
-      openGraph={{
-        type: 'article',
-        images: [
-          {
-            url: ogpImageUrl,
+}) => {
+  const canonicalUrl = `https://blog.ciffelia.com/article/${slug}`;
+
+  return (
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={canonicalUrl}
+        openGraph={{
+          type: 'article',
+          images: [
+            {
+              url: ogpImageUrl,
+            },
+          ],
+          article: {
+            publishedTime: publishedAt.toISOString(),
+            modifiedTime: modifiedAt.toISOString(),
+            authors: ['Ciffelia'],
+            tags,
           },
-        ],
-        article: {
-          publishedTime: publishedAt.toISOString(),
-          modifiedTime: modifiedAt.toISOString(),
-          authors: ['Ciffelia'],
-          tags,
-        },
-      }}
-    />
-  </>
-);
+        }}
+      />
+
+      <Head>
+        <script
+          {...jsonLdScriptProps<BreadcrumbList>({
+            '@context': 'https://schema.org',
+            '@type': 'BreadcrumbList',
+            itemListElement: [
+              {
+                '@type': 'ListItem',
+                position: 1,
+                name: title,
+                item: canonicalUrl,
+              },
+            ],
+          })}
+        />
+        <script
+          {...jsonLdScriptProps<BlogPosting>({
+            '@context': 'https://schema.org',
+            '@type': 'BlogPosting',
+            headline: title,
+            datePublished: publishedAt.toISOString(),
+            dateModified: modifiedAt.toISOString(),
+            image: [ogpImageUrl],
+          })}
+        />
+      </Head>
+    </>
+  );
+};
 
 export default ArticleHead;
