9d770028d3ac4217d2fdf52b231ce606b377cf5c
Ciffelia <mc.prince.0203@gmail.com>
2021-12-13 00:38:12 +0900

    Add shadow to SocialLink

--- src/components/app/header/SocialLink.tsx
+++ src/components/app/header/SocialLink.tsx
@@ -11,7 +11,7 @@ const SocialLink: React.VFC<Props> = ({ url, color, children }) => (
   <ExternalLink to={url}>
     <div
       style={{ backgroundColor: color }}
-      className="flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14 rounded-full"
+      className="flex items-center justify-center w-12 h-12 sm:w-14 sm:h-14 rounded-full hover:shadow-md hover:shadow-white/20 transition-all duration-200"
     >
       <div className="relative w-8 h-8">{children}</div>
     </div>
