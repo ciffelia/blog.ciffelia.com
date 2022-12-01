2b33f5c07a786988e960b581744e2082bcdd2806
Ciffelia <mc.prince.0203@gmail.com>
2022-12-02 00:07:21 +0900

    Update copyright

--- src/components/app/footer/Footer.tsx
+++ src/components/app/footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => (
   <footer
     className={`p-10 text-center ${quicksand.className} text-2xl text-gray-50 opacity-80`}
   >
-    <p>Copyright (c) 2021 Ciffelia</p>
+    <p>Copyright (c) 2021-2022 Ciffelia</p>
   </footer>
 );
 
