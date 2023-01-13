fb756577b7d83a4f610b9625346acb638307c770
Ciffelia <mc.prince.0203@gmail.com>
2023-01-13 18:52:59 +0900

    最終更新日の表示を追加

--- src/features/article/components/ArticleHeader.tsx
+++ src/features/article/components/ArticleHeader.tsx
@@ -1,5 +1,5 @@
-import { parseISO } from 'date-fns';
-import { FaPencilRuler, FaRegEdit } from 'react-icons/fa';
+import { isSameDay, parseISO } from 'date-fns';
+import { FaPencilRuler, FaRegEdit, FaSync } from 'react-icons/fa';
 import { ArticleMetadata } from '@/features/article';
 import { TagList } from '@/components/TagList';
 import { Timestamp } from '@/components/Timestamp';
@@ -9,24 +9,35 @@ export interface ArticleHeaderProps {
 }
 
 export const ArticleHeader: React.FC<ArticleHeaderProps> = ({
-  metadata: { title, isPublished, publishedAt, tags },
-}) => (
-  <div className="flex flex-col gap-y-5">
-    <div className="prose">
-      <h1 className="text-center">{title}</h1>
-    </div>
-    <TagList tags={tags} />
-    <div className="flex justify-center items-center gap-x-4">
-      {!isPublished && (
-        <div className="flex justify-center items-center gap-x-2 text-red-500">
-          <FaPencilRuler />
-          <span>下書き</span>
+  metadata: { title, isPublished, publishedAt, modifiedAt, tags },
+}) => {
+  const publishedAtDate = parseISO(publishedAt);
+  const modifiedAtDate = parseISO(modifiedAt);
+
+  return (
+    <div className="flex flex-col gap-y-5">
+      <div className="prose">
+        <h1 className="text-center">{title}</h1>
+      </div>
+      <TagList tags={tags} />
+      <div className="flex justify-center items-center gap-x-4">
+        {!isPublished && (
+          <div className="flex justify-center items-center gap-x-2 text-red-500">
+            <FaPencilRuler />
+            <span>下書き</span>
+          </div>
+        )}
+        <div className="flex justify-center items-center gap-x-2">
+          <FaRegEdit />
+          <Timestamp value={publishedAtDate} dateOnly />
         </div>
-      )}
-      <div className="flex justify-center items-center gap-x-2">
-        <FaRegEdit />
-        <Timestamp value={parseISO(publishedAt)} dateOnly />
+        {!isSameDay(publishedAtDate, modifiedAtDate) && (
+          <div className="flex justify-center items-center gap-x-2">
+            <FaSync />
+            <Timestamp value={modifiedAtDate} dateOnly />
+          </div>
+        )}
       </div>
     </div>
-  </div>
-);
+  );
+};
