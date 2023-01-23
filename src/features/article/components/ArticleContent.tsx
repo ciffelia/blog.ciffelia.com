da779afa061a2357b508a9fbf68fd435b0134f6c
Ciffelia <mc.prince.0203@gmail.com>
2023-01-23 18:28:12 +0900

    内部リンクのSPA遷移に対応

--- src/features/article/components/ArticleContent.tsx
+++ src/features/article/components/ArticleContent.tsx
@@ -1,8 +1,10 @@
 import React, { useMemo } from 'react';
+import Link from 'next/link';
 import Image, { StaticImageData } from 'next/image';
 import type { Root as HastRoot } from 'hast';
 import { unified } from 'unified';
 import rehypeReact, { Options as RehypeReactOptions } from 'rehype-react';
+import { isValidUrl } from '@/utils/isValidUrl';
 import { Embed } from './Embed';
 
 export interface ArticleContentProps {
@@ -16,6 +18,7 @@ export const ArticleContent: React.FC<ArticleContentProps> = ({ tree }) => {
       Fragment: React.Fragment,
       components: {
         pre: PreOverride,
+        a: AOverride,
       },
     };
     // @ts-expect-error
@@ -37,6 +40,18 @@ const PreOverride: React.FC<React.ComponentPropsWithoutRef<'pre'>> = (
       <pre {...props} />
     </div>
   );
+};
+
+const AOverride: React.FC<React.ComponentPropsWithoutRef<'a'>> = (props) => {
+  if (props.href === undefined || props.href.startsWith('#')) {
+    return <a {...props} />;
+  }
+
+  if (isValidUrl(props.href)) {
+    return <a target="_blank" {...props} />;
+  } else {
+    return <Link href={props.href}>{props.children}</Link>;
+  }
 };
 
 const CustomImage: React.FC<{
