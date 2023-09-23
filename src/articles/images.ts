bc803dae19134234b4044da49b7b70fa25eb3575
Ciffelia <mc.prince.0203@gmail.com>
2023-09-23 18:18:46 +0900

    記事「SECCON CTF 2023 Quals Writeup」を追加

--- src/articles/images.ts
+++ src/articles/images.ts
@@ -1,2 +1,3 @@
 export { default as coinMedalGold } from './contents/image/coinMedalGold.png';
 export { default as testArticleThumbnail } from './contents/image/testArticleThumbnail.jpg';
+export { default as seccon2023QualsResult } from './contents/image/seccon2023QualsResult.png';
