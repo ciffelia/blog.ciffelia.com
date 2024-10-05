aa1a4163c53b0fd010f57e964ab7454b0a0668ec
ciffelia <15273128+ciffelia@users.noreply.github.com>
2024-10-05 16:56:20 +0900

    add index page

---
+++ src/constant.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = 'blog.ciffelia.com'
+
+export const SITE_DESCRIPTION =
+  'Ciffeliaが何でも書くブログ。ソフトウェア開発、電子工作、近況、今年の振り返りなど。'
---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      // https://ics.media/entry/200317/
+      serif: `"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", 
+    "BIZ UDPGothic", Meiryo, sans-serif`,
+    },
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none', // Unset max-width: 65ch on .prose
+            fontSize: '16px',
+            letterSpacing: '0.1em',
+            textAlign: 'justify',
+            wordBreak: 'break-all',
+          },
+        },
+      },
+      aspectRatio: {
+        ogp: '1.91 / 1',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
